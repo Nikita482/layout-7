2648af15728c557f03145f4e650fe545c05f8353
Nikita <nekitakytilov@gmail.com>
2025-03-18 02:47:16 +0300

    test img js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import '../scss/style.scss'
 import { info } from './layout/info'
+import logo from '../img/header/logo.png' // Путь на уровень выше
+
+const img = document.getElementById('logo')
+img.src = logo // путь к картинке
 
 info()
