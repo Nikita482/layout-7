e7801bcbc6fa631de906974c0b611a1ed71f7548
Nikita <nekitakytilov@gmail.com>
2025-03-19 15:42:57 +0300

    brands

--- src/js/layout/brands.js
+++ src/js/layout/brands.js
@@ -26,4 +26,41 @@ export function brands() {
       }
     }
   })
+
+  let active = true
+  const logo = document.getElementById('all-logo')
+  logo.innerText = 'Показать все'
+
+  logo.addEventListener('click', () => {
+    const slides = document.querySelectorAll('.swiper-slide:nth-child(n + 7)')
+    const slides7 = document.querySelectorAll('.swiper-slide:nth-child(7)')
+    const slides8 = document.querySelectorAll('.swiper-slide:nth-child(8)')
+
+    if (active) {
+      slides.forEach((slide) => {
+        slide.style.display = 'flex'
+      })
+
+      logo.innerText = 'Скрыть'
+
+      active = !active
+    } else {
+      slides.forEach((slide) => {
+        slide.style.display = 'none'
+      })
+
+      if (window.matchMedia('(min-width: 1120px)').matches) {
+        slides7.forEach((slide) => {
+          slide.style.display = 'flex'
+        })
+        slides8.forEach((slide) => {
+          slide.style.display = 'flex'
+        })
+      }
+
+      logo.innerText = 'Показать все'
+
+      active = !active
+    }
+  })
 }
